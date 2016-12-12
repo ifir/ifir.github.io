71b6f4b4d1c85217badbe441818f59e59177e6c2
fir <664339636@qq.com>
2016-12-12 14:39:42 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 window.addEventListener('load', load, false);
 
 function load() {
-	utils.$('你好');
+	//loading
+	var timer = null;
+	var $loading = utils.$('.loading')[0];
+	utils.addClass($loading, 'loaded');
+	clearTimeout(timer);
+	timer = setTimeout(function(){
+		//移除loading节点
+		$loading.remove();
+	},5000);
 }
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,20 @@
+'use strict';
 var utils = {};
 //获取dom
 utils.$ = function(selector){
-	console.log(selector)
+	return document.querySelectorAll(selector);
+}
+utils.addClass = function(ele, cls){
+	var className = ele.className;
+	ele.className = className + ' ' + cls;
+}
+utils.removeClass = function(ele, cls){
+	if(ele.className === '') return;
+	var clsArr = ele.className.split(' ');
+	var clsObj = {};
+	for(var i = 0, len = clsArr.length; i < len; i++){
+		clsObj[clsArr[i]] = i;
+	}
+	clsArr.splice(clsObj[cls],1);
+	ele.className = clsArr.join(' ');
 }
