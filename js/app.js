a13f07b243af196bf349519d581be16eecdc6b3d
fir <664339636@qq.com>
2016-12-13 11:24:30 +0800

    update

--- js/app.js
+++ js/app.js
@@ -5,16 +5,48 @@ function load() {
 	var timer = null;
 	var $loading = utils.$('.loading')[0];
 	var $pageone = utils.$('.section-pageone')[0];
+	var height = window.innerHeight;
+	var page = 0;
+	var isWheel = false, isComplete = false, wheelTimer = null;
 	utils.addClass($loading, 'loaded');
 	clearTimeout(timer);
 	timer = setTimeout(function(){
 		//移除loading节点
 		$loading.remove();
 		utils.addClass($pageone, 'pageone-ani');
+		clearTimeout(timer);
+		//鼠标滑轮绑定事件
+		window.addEventListener('mousewheel', function(e){
+			if(e.deltaY > 0 && page < 1){
+				nextPage();
+				console.log('向下')
+			}
+			if(e.deltaY < 0 && page > 0){
+				prevPage();
+				console.log('向上')
+			}
+		})
 	},4001);
 	//第一页
+	utils.$('.next-pagetwo')[0].addEventListener('click', nextPage,false);
+	function nextPage(){
+		page++;
+		utils.$('.container')[0].style.transform = "translateY(" + -height*page + "px)";
+		utils.$('.container')[0].style.webkitTransform = "translateY(" + -height*page + "px)";
+	}
+	function prevPage(){
+		page--;
+		utils.$('.container')[0].style.transform = "translateY(" + height*page + "px)";
+		utils.$('.container')[0].style.webkitTransform = "translateY(" + height*page + "px)";
+	}
+	var isScroll = false;
+	// window.addEventListener('scroll', function(){
+	// 	var sTop = document.body.scrollTop || document.documentElement.scrollTop;
+	// 	if(sTop > 0){
+	// 		isScroll = true;
 
-
-
+	// 	}
+	// }, false)
+	
 
 }
