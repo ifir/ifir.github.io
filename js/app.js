cc29813223ab9063dd390cfc51f09bf0ae1c070c
fir <664339636@qq.com>
2016-12-14 12:40:42 +0800

    update

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ function load() {
 	var page = 0, translate = 0;
 	var pageNum = utils.$('.page').length - 1;
 	var isWheel = false, isComplete = false, wheelTimer = null;
+	//触摸所需变量
+	var startX = 0, startY = 0,
+		offsetX = 0, offsetY = 0,
+		isTouch = false, isMove = false,
+		startTime = 0, endTime = 0;
 	utils.addClass($loading, 'loaded');
 	clearTimeout(timer);
 	timer = setTimeout(function(){
@@ -25,10 +30,16 @@ function load() {
 				prevPage();
 			}
 		});
+		//触摸事件绑定
+		utils.$('body')[0].addEventListener('touchstart', touchstart, false);
+		utils.$('body')[0].addEventListener('touchmove', touchmove, false);
+		utils.$('body')[0].addEventListener('touchend', touchend, false);
 	},4001);
 	//第一页
 	utils.$('.next-pagetwo')[0].addEventListener('click', nextPage,false);
 	utils.$('.next-pagethree')[0].addEventListener('click', nextPage,false);
+	//第二页
+	//下一页切换事件
 	function nextPage(){
 		if(page === pageNum) return;
 		page++;
@@ -41,6 +52,7 @@ function load() {
 		}
 		utils.addClass($page[page], 'active');
 	}
+	//上一页切换事件
 	function prevPage(){
 		if(page === 0) return;
 		page--;
@@ -53,21 +65,7 @@ function load() {
 		}
 		utils.addClass($page[page], 'active');
 	}
-	//第二页
-	//
-	//
-	//
-	//
-	//触摸滑动
-	var startX = 0, startY = 0,
-		offsetX = 0, offsetY = 0,
-		isTouch = false, isMove = false,
-		startTime = 0, endTime = 0;
-	utils.$('body')[0].addEventListener('touchstart', touchstart, false);
-	utils.$('body')[0].addEventListener('touchmove', touchmove, false);
-	utils.$('body')[0].addEventListener('touchend', touchend, false);
-
-
+	//触摸事件
 	function touchstart(e){
 		startTime = Date.now();
 		isTouch = true;
@@ -84,18 +82,24 @@ function load() {
 	function touchend(e){
 		if(!isMove) return;
 		endTime = Date.now();
+		var timer = null;
 		var touchTime = endTime - startTime;
 		isTouch = false;
 		//判断是否为上下滑动
 		//向上滑动
+		clearTimeout(timer);
 		if(Math.abs(offsetY) > 10 && offsetY < 0){
-			nextPage();
-			console.log('向上滑动')
+			timer = setTimeout(function(){
+				nextPage();
+				clearTimeout(timer);
+			},10);
 		}
 		//向下滑动
 		if(Math.abs(offsetY) > 10 && offsetY > 0){
-			prevPage();
-			console.log('向下滑动')
+			timer = setTimeout(function(){
+				prevPage();
+				clearTimeout(timer);
+			},10);
 		}
 	}
 
