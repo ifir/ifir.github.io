8d49dfaf1ddf4ee558207615ed4d37f4a18c749c
fir <664339636@qq.com>
2016-12-14 11:27:01 +0800

    touch

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function load() {
 	var $pageone = utils.$('.section-pageone')[0];
 	var height = window.innerHeight;
 	var page = 0, translate = 0;
+	var pageNum = utils.$('.page').length - 1;
 	var isWheel = false, isComplete = false, wheelTimer = null;
 	utils.addClass($loading, 'loaded');
 	clearTimeout(timer);
@@ -17,20 +18,19 @@ function load() {
 		clearTimeout(timer);
 		//鼠标滑轮绑定事件
 		window.addEventListener('mousewheel', function(e){
-			if(e.deltaY > 0 && page < 2){
+			if(e.deltaY > 0 && page < pageNum){
 				nextPage();
-				console.log('向下')
 			}
 			if(e.deltaY < 0 && page > 0){
 				prevPage();
-				console.log('向上')
 			}
 		});
-	},4001);
+	},1);
 	//第一页
 	utils.$('.next-pagetwo')[0].addEventListener('click', nextPage,false);
 	utils.$('.next-pagethree')[0].addEventListener('click', nextPage,false);
 	function nextPage(){
+		if(page === pageNum) return;
 		page++;
 		translate -= height;
 		var $page = utils.$('.page');
@@ -43,6 +43,7 @@ function load() {
 		utils.addClass($page[page], 'active');
 	}
 	function prevPage(){
+		if(page === 0) return;
 		page--;
 		translate += height;
 		var $page = utils.$('.page');
@@ -59,14 +60,45 @@ function load() {
 	//
 	//
 	//触摸滑动
-	var startX = 0, startY = 0;
-	utils.$('.container')[0].addEventListener(_this.touchstart, touchstart, false);
-	utils.$('.container')[0].addEventListener(_this.touchmove, touchmove, false);
-	utils.$('.container')[0].addEventListener(_this.touchend, touchend, false);
+	var startX = 0, startY = 0,
+		offsetX = 0, offsetY = 0,
+		isTouch = false, isMove = false,
+		startTime = 0, endTime = 0;
+	utils.$('.container')[0].addEventListener('touchstart', touchstart, false);
+	utils.$('.container')[0].addEventListener('touchmove', touchmove, false);
+	utils.$('.container')[0].addEventListener('touchend', touchend, false);
 
 
 	function touchstart(e){
-		
+		startTime = Date.now();
+		isTouch = true;
+		//startX = e.touches[0].pageX;
+		startY = e.touches[0].pageY;
+		console.log('touchstart')
+	}
+	function touchmove(e){
+		if(!isTouch) return;
+		//offsetX = e.touches[0].pageX - startX;
+		offsetY = e.touches[0].pageY - startY;
+		//判断是否为真的移动了
+		console.log(offsetY)
+		isMove = Math.abs(offsetY) > 11 ? true : false;
+	}
+	function touchend(e){
+		if(!isMove) return;
+		endTime = Date.now();
+		var touchTime = endTime - startTime;
+		//判断是否为上下滑动
+		//向上滑动
+		if(Math.abs(offsetY) > 10 && offsetY < 0){
+			nextPage();
+			console.log('向上滑动')
+		}
+		//向下滑动
+		if(Math.abs(offsetY) > 10 && offsetY > 0){
+			prevPage();
+			console.log('向下滑动')
+		}
 	}
 
 
