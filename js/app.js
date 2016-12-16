86eb6a0628ae8b3a50a1da7cc5514fbece3c5b6d
fir <664339636@qq.com>
2016-12-16 10:35:51 +0800

    optimize

--- js/app.js
+++ js/app.js
@@ -6,42 +6,16 @@ function load() {
 	var timer = null;
 	var $loading = utils.$('.loading')[0];
 	var $pageone = utils.$('.section-pageone')[0];
+	var $radarBox = utils.$('.radar-box')[0];
 	var height = window.innerHeight;
 	var page = 0, translate = 0;
 	var pageNum = utils.$('.page').length - 1;
-	var isWheel = false, isComplete = false, wheelTimer = null;
 	//触摸所需变量
 	var startX = 0, startY = 0,
 		offsetX = 0, offsetY = 0,
 		isTouch = false, isMove = false,
 		startTime = 0, endTime = 0;
-	clearTimeout(timer);
-	timer = setTimeout(function(){
-		//移除loading节点
-		$loading.remove();
-		utils.addClass($pageone, 'active');
-		clearTimeout(timer);
-		//鼠标滑轮绑定事件
-		window.addEventListener('mousewheel', function(e){
-			if(e.deltaY > 0 && page < pageNum){
-				nextPage();
-			}
-			if(e.deltaY < 0 && page > 0){
-				prevPage();
-			}
-		});
-		//触摸事件绑定
-		utils.$('body')[0].addEventListener('touchstart', touchstart, false);
-		utils.$('body')[0].addEventListener('touchmove', touchmove, false);
-		utils.$('body')[0].addEventListener('touchend', touchend, false);
-	},3000);
-	//每一页下的小箭头绑定事件
-	for(var j = 0; j < pageNum; j++){
-		utils.$('.next')[j].addEventListener('click', nextPage, false);
-	}
-	utils.$('.prev')[0].addEventListener('click', prevPage, false);
-	//雷达图
-	var $radarBox = utils.$('.radar-box')[0];
+	//雷达图数据
 	var radarData = [
 		['Gulp/Webpack', 0.75],
 		['React/Vue', 0.7],
@@ -51,71 +25,94 @@ function load() {
 		['PS/AI', 0.7],
 		['Jquery/Zepto', 0.85]
 	];
+	//雷达图所需变量
+	var center = [200, 200], r = 200, cvsW = 400, cvsH = 400, dataLen = radarData.length;
+	clearTimeout(timer);//清除loading的定时器
+	timer = setTimeout(function(){
+		//移除loading节点
+		$loading.remove();
+		utils.addClass($pageone, 'active');
+		//鼠标滑轮绑定事件
+		window.addEventListener('mousewheel', function(e){
+			if(e.deltaY > 0 && page < pageNum){
+				nextPage(radarAntimate);
+			}
+			if(e.deltaY < 0 && page > 0){
+				prevPage(radarAntimate);
+			}
+		});
+		var $body = utils.$('body')[0];
+		//触摸事件绑定
+		$body.addEventListener('touchstart', touchstart, false);
+		$body.addEventListener('touchmove', touchmove, false);
+		$body.addEventListener('touchend', touchend, false);
+		clearTimeout(timer);//清除loading的定时器
+	},3000);
+	//每一页下的小箭头绑定事件
+	for(var j = 0; j < pageNum; j++){
+		utils.$('.next')[j].addEventListener('click', nextPage, false);
+	}
+	//最后一页向上的小箭头
+	utils.$('.prev')[0].addEventListener('click', prevPage, false);
 	//绘制雷达图背景
-	var cvsbg = document.createElement('canvas');
-	var ctxbg = cvsbg.getContext('2d');
-	cvsbg.width = 400;
-	cvsbg.height = 400;
-	$radarBox.appendChild(cvsbg);
-
-	//计算多边形顶点坐标
-	// X坐标 = 圆心的x坐标 + Math.sin(弧度) * 半径;
-	// Y坐标 = 圆心的y坐标 + Math.cos(弧度) * 半径;
-	// 弧度 = (2*Math.PI/360) * (365/多边形的定点数) * 多边形第几个顶点;
-	for(var s = 5; s > 0; s--){
-		ctxbg.beginPath();
-		for(var k = 0, len = radarData.length; k < len; k++){
-			var rad = (2*Math.PI/360) * (365/len) * k;
-			var x = 200 + Math.sin(rad) * 200 * (s/5);
-			var y = 200 + Math.cos(rad) * 200 * (s/5);
-			ctxbg.lineTo(x,y);
-		}
-		ctxbg.closePath();
-		if(s%2){
-			ctxbg.fillStyle = '#d3c9c4';
-		}else{
-			ctxbg.fillStyle = '#d9d6d5';
-		}
-		ctxbg.fill();
-	}
-	ctxbg.beginPath();
-	//绘制伞骨
-	for(var n = 0, len = radarData.length; n < len; n++){
-
-		var rad = (2*Math.PI/360) * (365/len) * n;
-		var x = 200 + Math.sin(rad) * 200;
-		var y = 200 + Math.cos(rad) * 200;
-		ctxbg.moveTo(200, 200);
-		ctxbg.lineTo(x, y)
-		//绘制文字
-		var span = document.createElement('span');
-		span.innerHTML = radarData[n][0];
-		//判断文字左右
-		x > 400/2 ? (span.style.left = x/2 + 'px') : (span.style.right = (400 - x)/2 + 'px');
-		//判断文字上下
-		y > 400/2 ? (span.style.top = y/2 + 'px') : (span.style.bottom = ((400 - y)/2) + 'px');
-		if(x === 400/2 ){span.style.left = (x/2 - 50) + 'px';}
-		$radarBox.appendChild(span);
-	}
-	ctxbg.strokeStyle = '#e5dedb';
-	ctxbg.stroke();
-
+	renderRadarBg();
 	//绘制雷达图数据图
 	var cvsdata = document.createElement('canvas');
 	var ctxdata = cvsdata.getContext('2d');
-	cvsdata.width = 400;
-	cvsdata.height = 400;
+	cvsdata.width = cvsW;
+	cvsdata.height = cvsH;
 	$radarBox.appendChild(cvsdata);
+	//绘制雷达图背景函数
+	function renderRadarBg(){
+		var cvsbg = document.createElement('canvas');
+		var ctxbg = cvsbg.getContext('2d');
+		cvsbg.width = cvsW;
+		cvsbg.height = cvsH;
+		$radarBox.appendChild(cvsbg);
+		for(var s = 5; s > 0; s--){
+			ctxbg.beginPath();
+			for(var k = 0; k < dataLen; k++){
+				var rate = s/5;
+				//计算多边形顶顶点{x,y}坐标
+				var point = calcPolygon(dataLen, k, center[0], center[1], r, rate);
+				ctxbg.lineTo(point.x,point.y);
+			}
+			ctxbg.closePath();
+			if(s%2){
+				ctxbg.fillStyle = '#d3c9c4';
+			}else{
+				ctxbg.fillStyle = '#d9d6d5';
+			}
+			ctxbg.fill();
+		}
+		ctxbg.beginPath();
+		//绘制伞骨
+		for(var n = 0; n < dataLen; n++){
+			var point = calcPolygon(dataLen, n, center[0], center[1], r, 1);
+			ctxbg.moveTo(center[0], center[1]);
+			ctxbg.lineTo(point.x, point.y);
+			//绘制文字
+			var span = document.createElement('span');
+			span.innerHTML = radarData[n][0];
+			//判断文字左右
+			point.x > cvsW/2 ? (span.style.left = point.x/2 + 'px') : (span.style.right = (cvsW - point.x)/2 + 'px');
+			//判断文字上下
+			point.y > cvsH/2 ? (span.style.top = point.y/2 + 'px') : (span.style.bottom = ((cvsH - point.y)/2) + 'px');
+			if(point.x === cvsW/2 ){span.style.left = (point.x/2 - 50) + 'px';}
+			$radarBox.appendChild(span);
+		}
+		ctxbg.strokeStyle = '#e5dedb';
+		ctxbg.stroke();
+	}
+	//绘制雷达折线图
 	function renderRadar(per){
-		ctxdata.clearRect(0,0,400,400);
+		ctxdata.clearRect(0,0,cvsW,cvsH);
 		//绘制折线
 		ctxdata.beginPath();
-		for(var n = 0, len = radarData.length; n < len; n++){
+		for(var n = 0; n < dataLen; n++){
 			var rate = radarData[n][1] * per;
-			var rad = (2*Math.PI/360) * (365/len) * n;
-			var x = 200 + Math.sin(rad) * 200 * rate;
-			var y = 200 + Math.cos(rad) * 200 * rate;
-			ctxdata.lineTo(x, y);
+			var point = calcPolygon(dataLen, n, center[0], center[1], r, rate);
+			ctxdata.lineTo(point.x, point.y);
 		}
 		ctxdata.closePath();
 		ctxdata.fillStyle = 'rgba(241, 212, 199, 0.35)';
@@ -123,56 +120,56 @@ function load() {
 		ctxdata.lineWidth = 4;
 		ctxdata.strokeStyle = "#a19088";
 		ctxdata.stroke();
-
-
 		//绘制小圆点
-		for(var j = 0, len = radarData.length; j < len; j++){
+		for(var j = 0; j < dataLen; j++){
 			var rate = radarData[j][1] * per;
-			var rad = (2*Math.PI/360) * (365/len) * j ;
-			var x = 200 + Math.sin(rad) * 200 * rate;
-			var y = 200 + Math.cos(rad) * 200 * rate;
+			var point = calcPolygon(dataLen, j, center[0], center[1], r, rate);
 			ctxdata.beginPath();
-			ctxdata.arc(x, y, 5, 0, Math.PI*2);
+			ctxdata.arc(point.x, point.y, 5, 0, Math.PI*2);
 			ctxdata.closePath();
 			ctxdata.fillStyle = '#919191';
 			ctxdata.fill();
 		}
 	}
+	/**
+	 * [calcPolygon description]计算多边形顶点坐标
+	 * @param  {[type]} apexNum 多边形的顶点个数
+	 * @param  {[type]} index 	当前第几个顶点
+	 * @param  {[type]} centerX 圆心x坐标
+	 * @param  {[type]} centerY 圆心y坐标
+	 * @param  {[type]} r 		圆的半径
+	 * @param  {[type]} rate    比率
+	 * @return {[type]}         返回{x,y}
+	 */
+	function calcPolygon(apexNum, index, centerX, centerY, r, rate){
+		//计算多边形顶点坐标
+		// X坐标 = 圆心的x坐标 + Math.sin(弧度) * 半径;
+		// Y坐标 = 圆心的y坐标 + Math.cos(弧度) * 半径;
+		// 弧度 = (2*Math.PI/360) * (365/多边形的定点数) * 多边形第几个顶点;
+		var point = {};
+		var rad = (2*Math.PI/360) * (365/apexNum) * index;//弧度
+		point.x = centerX + Math.sin(rad) * r * rate;
+		point.y = centerY + Math.cos(rad) * r * rate;
+		return point;
+	}
 	//下一页切换事件
-	function nextPage(){
+	function nextPage(callback){
 		if(page === pageNum) return;
 		page++;
 		translate -= height;
-		var $page = utils.$('.page');
-		utils.$('.container')[0].style.transform = "translateY(" + translate + "px)";
-		utils.$('.container')[0].style.webkitTransform = "translateY(" + translate + "px)";
-		for(var i = 0, len = $page.length; i < len; i++){
-			utils.removeClass($page[i], 'active');
-		}
-		utils.addClass($page[page], 'active');
-		var timeid = null;
-		if(utils.hasClass(utils.$('.section-pagethree')[0], 'active')){
-			var speed = 0;
-			for(var u = 0; u < 100; u++){
-				speed += 0.01;
-				(function(speed){
-					timeid = setTimeout(function(){
-						renderRadar(speed);
-					},u*10+500);
-				})(speed);
-			}
-		}else{
-			timeid = setTimeout(function(){
-				renderRadar(0);
-			},500);
-		}
-		clearTimeout(timeid);
+		pageTransform(page, translate);
+		callback && callback();
 	}
 	//上一页切换事件
-	function prevPage(){
+	function prevPage(callback){
 		if(page === 0) return;
 		page--;
 		translate += height;
+		pageTransform(page, translate);
+		callback && callback();
+	}
+	//添加page切换的transform动画
+	function pageTransform(page, translate){
 		var $page = utils.$('.page');
 		utils.$('.container')[0].style.transform = "translateY(" + translate + "px)";
 		utils.$('.container')[0].style.webkitTransform = "translateY(" + translate + "px)";
@@ -180,6 +177,9 @@ function load() {
 			utils.removeClass($page[i], 'active');
 		}
 		utils.addClass($page[page], 'active');
+	}
+	//雷达图动画
+	function radarAntimate(){
 		var timeid = null;
 		if(utils.hasClass(utils.$('.section-pagethree')[0], 'active')){
 			var speed = 0;
@@ -201,12 +201,10 @@ function load() {
 	//触摸事件
 	function touchstart(e){
 		isTouch = true;
-		//startX = e.touches[0].pageX;
 		startY = e.touches[0].pageY;
 	}
 	function touchmove(e){
 		if(!isTouch) return;
-		//offsetX = e.touches[0].pageX - startX;
 		offsetY = e.touches[0].pageY - startY;
 		//判断是否为真的移动了
 		isMove = Math.abs(offsetY) > 11 ? true : false;
@@ -222,18 +220,16 @@ function load() {
 		clearTimeout(timer);
 		if(Math.abs(offsetY) > 10 && offsetY < 0){
 			timer = setTimeout(function(){
-				nextPage();
+				nextPage(radarAntimate);
 				clearTimeout(timer);
 			},10);
 		}
 		//向下滑动
 		if(Math.abs(offsetY) > 10 && offsetY > 0){
 			timer = setTimeout(function(){
-				prevPage();
+				prevPage(radarAntimate);
 				clearTimeout(timer);
 			},10);
 		}
 	}
-
-
 }
--- js/utils.js
+++ js/utils.js
@@ -27,29 +27,3 @@ utils.hasClass = function(ele, cls){
 		return new RegExp('(^| )' + cls + '( |$)', 'gi').test(ele.className);
 	}
 }
-
-// utils.animate = function(){
-//     var lastTime = 0;
-//     var vendors = ['webkit', 'moz', 'ms', 'o'];
-//     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-//         return window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-//         window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-//     }
-//     if(!window.requestAnimationFrame){
-//         return window.requestAnimationFrame = function(callback, element) {
-//             var currTime = new Date().getTime();
-//             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-//             var id = window.setTimeout(function() {
-//             	callback(currTime + timeToCall);
-//             },timeToCall);
-//             lastTime = currTime + timeToCall;
-//             return id;
-//         };
-//     }
-
-//     if(!window.cancelAnimationFrame){
-//         window.cancelAnimationFrame = function(id) {
-//             clearTimeout(id);
-//         };
-//     }
-// }
