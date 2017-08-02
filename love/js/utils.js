6d74991506f3e0d4a24f979cd25a846654560933
zhangzhenlin01 <zhangzhenlin01@58ganji.com>
2017-08-02 09:56:50 +0800

    update

---
+++ love/js/utils.js
@@ -0,0 +1,30 @@
+'use strict';
+var utils = {};
+//获取dom
+utils.$ = function(selector) {
+    return document.querySelectorAll(selector);
+}
+utils.addClass = function(ele, cls) {
+    if (ele.classList) {
+        ele.classList.add(cls);
+    } else {
+        ele.className += ' ' + cls;
+    }
+}
+utils.removeClass = function(ele, cls) {
+    if (ele.classList) {
+        ele.classList.remove(cls);
+    } else {
+        ele.className = ele.className.replace(new RegExp('(^|\\b)' + cls.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+}
+utils.hasClass = function(ele, cls) {
+    if (ele.classList) {
+        return ele.classList.contains(cls);
+    } else {
+        return new RegExp('(^| )' + cls + '( |$)', 'gi').test(ele.className);
+    }
+}
+utils.remove = function(ele) {
+    ele.parentNode.removeChild(ele);
+}
