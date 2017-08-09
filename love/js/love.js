63cb50412c6426b5cac6ff514a4b91f06ebfa1a3
zhangzhenlin01 <zhangzhenlin01@58ganji.com>
2017-08-09 11:36:30 +0800

    update

--- love/js/love.js
+++ love/js/love.js
@@ -8,13 +8,10 @@
         var WINDOW_HEIGHT = w.innerHeight;
     }
     //移动端不支持自动播放
-    utils.$('#bgm')[0].onloadedmetadata = function(){
-        alert(1)
-        d.documentElement.addEventListener('touchstart', bgmAutoPlay, false);
-        function bgmAutoPlay(){
-            alert(2)
-            utils.$('#bgm')[0].play();
-            d.documentElement.removeEventListener('touchstart', bgmAutoPlay);
-        }
+    d.documentElement.addEventListener('touchstart', bgmAutoPlay, false);
+    function bgmAutoPlay(){
+        alert(2)
+        utils.$('#bgm')[0].play();
+        d.documentElement.removeEventListener('touchstart', bgmAutoPlay);
     }
 })(window, document);
