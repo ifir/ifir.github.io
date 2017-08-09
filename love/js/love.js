35eb2eb497d7cf804753c88dffe13252a91bc86d
zhangzhenlin01 <zhangzhenlin01@58ganji.com>
2017-08-09 10:20:21 +0800

    update

--- love/js/love.js
+++ love/js/love.js
@@ -6,16 +6,5 @@
         //常量
         var WINDOW_WIDTH = w.innerWidth;
         var WINDOW_HEIGHT = w.innerHeight;
-        initLoadingCvs();
-        //初始化loading
-        function initLoadingCvs(){
-            var cvs = d.getElementById('love-loading');
-            var ctx = cvs.getContext('2d');
-            cvs.width = WINDOW_WIDTH;
-            cvs.height = WINDOW_HEIGHT;
-            function drawLoading(){
-                ctx.beginPath();
-            }
-        }
     }
 })(window, document);
