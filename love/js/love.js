f88da80f8fcd8382d0d3caa28e15faca04b14200
zhangzhenlin01 <zhangzhenlin01@58ganji.com>
2017-08-15 14:45:27 +0800

    update

--- love/js/love.js
+++ love/js/love.js
@@ -1,51 +1,52 @@
 'use strict';
-(function(w, d){
+(function(w, d) {
     w.addEventListener('load', init, false);
     //初始化
-    function init(){
+    function init() {
         //常量
-        var WINDOW_WIDTH = w.innerWidth;
-        var WINDOW_HEIGHT = w.innerHeight;
+        var WINDOW_WIDTH = w.innerWidth,
+            WINDOW_HEIGHT = w.innerHeight;
 
-        var timer = null, timer2 = null;
+        var timer = null,
+            timer2 = null;
         //移动端不支持自动播放
         d.documentElement.addEventListener('touchstart', bgmAutoPlay, false);
         //水果切换
         utils.addClass(utils.$('.bubble-fantasy')[0], 'bubble-fantasy-scale');
-        timer = w.setTimeout(function(){
+        timer = w.setTimeout(function() {
             timer2 = w.setInterval(randomFood, 300);
             clearTimeout(timer);
         }, 2000);
         //资源加载完去掉loading
-        function sourceLoading(){
-            w.setTimeout(function(){
+        function sourceLoading() {
+            w.setTimeout(function() {
                 utils.addClass(utils.$('.loading-wrap')[0], 'loaded');
                 /*utils.remove(utils.$('.loading-wrap')[0]);
                 clearInterval(timer2);*/
-            }, 10000);
+            }, 2000);
         }
         sourceLoading();
 
 
     }
     //bgm 播放
-    function bgmAutoPlay(){
+    function bgmAutoPlay() {
         utils.$('#bgm')[0].play();
         d.documentElement.removeEventListener('touchstart', bgmAutoPlay);
     }
     //loading动画中水果随机切换
-    function randomFood(){
+    function randomFood() {
         var food = utils.$('.icon-food')[0];
         var cls = food.className.split(' ')[1];
         var index = Math.floor(Math.random() * 20);
-        if(cls){
+        if (cls) {
             utils.removeClass(food, cls);
             //防止随机出现两次一样的index
             var lastIndex = parseInt(cls.split('-')[2], 10);
-            if(lastIndex === index){
-                if(lastIndex === 22){
+            if (lastIndex === index) {
+                if (lastIndex === 22) {
                     index--;
-                }else{
+                } else {
                     index++;
                 }
             }
